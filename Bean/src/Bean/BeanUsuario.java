4c7132cdfe66aea78879c1d238cc6ae00a3bbb1e
javierdt <jdavila.soft@gmail.com>
2015-05-01 16:53:45 -0500

    Cambios en BeanUsuario

--- Bean/src/Bean/BeanUsuario.java
+++ Bean/src/Bean/BeanUsuario.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Bean;
 
 /**
@@ -11,10 +10,13 @@ package Bean;
  * @author Alumno
  */
 public class BeanUsuario extends BeanPersonal {
+
     private Integer userCodigo;
     private String userLogin;
     private String userEstado;
-   
+    private BeanPersonal personal;
+    private BeanSucursal sucursal;
+    private String contrasenia;
 
     public Integer getUserCodigo() {
         return userCodigo;
@@ -40,9 +42,28 @@ public class BeanUsuario extends BeanPersonal {
         this.userEstado = userEstado;
     }
 
-    
-    
-    
-    
-    
+    public BeanPersonal getPersonal() {
+        return personal;
+    }
+
+    public void setPersonal(BeanPersonal personal) {
+        this.personal = personal;
+    }
+
+    public BeanSucursal getSucursal() {
+        return sucursal;
+    }
+
+    public void setSucursal(BeanSucursal sucursal) {
+        this.sucursal = sucursal;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
 }
